adcdc07d406e3beeb6203abbb2456fbf8f3ab108
Dennis Benkert <spinecrasher@googlemail.com>
2015-11-27 13:41:11 +0100

    added setting of timezone to shell provisioner

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -2,6 +2,8 @@
 
 DEBIAN_FRONTEND=noninteractive
 
+sudo timedatectl set-timezone Europe/Berlin
+
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" > /etc/apt/sources.list.d/docker.list
 
