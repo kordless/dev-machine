4e022802ab757d08dad3b121637ace21e54f7e32
Dennis Benkert <spinecrasher@googlemail.com>
2015-12-11 11:07:29 -0800

    removed crap

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-LS_CONFIG=/etc/logstash-forwarder.conf
-sed -ie "s/%%LOGSTASH_ADDR%%/${LOGSTASH_ADDR}/g" $LS_CONFIG
-exec /opt/logstash-forwarder/bin/logstash-forwarder.sh -config $LS_CONFIG
